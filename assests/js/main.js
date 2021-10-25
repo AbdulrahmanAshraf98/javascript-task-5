2dca7755042616e91b3a1c9c3c7718128d620340
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2021-10-26 01:14:27 +0200

    first commit

---
+++ assests/js/main.js
@@ -0,0 +1,23 @@
+var grid=document.getElementById("grid");
+console.log(grid)
+var imagePath=["assests/img/1.webp","assests/img/2.webp","assests/img/3.webp","assests/img/4.webp","assests/img/5.webp","assests/img/6.webp","assests/img/7.webp","assests/img/8.webp","assests/img/9.webp"];
+var title=["minimal design","Paint wall","female light","fourth air","multiply fowl","together sign","green heaven","fly male","season face",]
+for(var i=0 ; i<9;i++)
+{
+    let item = document.createElement("div");
+    item.classList.add("item")
+    item.innerHTML=`
+    <div class="img">
+    <img src="${imagePath[i]}" alt="" srcset="">
+    <div class="overlay">   
+        <p>+</p>  
+    </div> 
+    </div>
+    <div class="content">
+    <h4>
+        <a>${title[i]}</a>
+    </h4>
+    <p>Animated, portfolio</p>
+    </div>`
+    grid.appendChild(item);
+}
